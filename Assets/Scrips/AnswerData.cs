f50bc3b080f9bf7e3a82e9dbfc9af55fe8de0edc
Guillaume Raymond <guillaumeraymond@hotmail.ca>
2016-12-08 23:43:06 -0500

    start menu Screen

--- Assets/Scrips/AnswerData.cs
+++ Assets/Scrips/AnswerData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class AnswerData : MonoBehaviour 
+public class AnswerData 
 {
 	public string answerText;
 	public bool isCorrect;
