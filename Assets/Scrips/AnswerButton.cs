9fd156e01b0cce50184e8c54a2a9cda59fe38bd0
Guillaume Raymond <guiray@users.noreply.github.com>
2016-12-19 21:44:22 -0500

    Answer Button

---
+++ Assets/Scrips/AnswerButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour {
+
+	public Text answerText;
+
+	private AnswerData answerData;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void Setup(AnswerData data){
+		answerData = data;
+
+		answerText.text = answerData.answerText;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
